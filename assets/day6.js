c06483f63b590c8e95dea587f2f280b661e748c1
Annisaa Utami <59133515+auttmme@users.noreply.github.com>
2021-06-28 22:58:24 +0700

    commit 1

---
+++ assets/day6.js
@@ -0,0 +1,39 @@
+function changeGambarJae1(){
+    document.getElementById("gambarJae").src="assets/image/jae1.jpg";
+}
+
+function changeGambarJae2(){
+    document.getElementById("gambarJae").src="assets/image/jae2.jpg";
+}
+
+function changeGambarSungjin1(){
+    document.getElementById("gambarSungjin").src="assets/image/sungjin1.jpg";
+}
+
+function changeGambarSungjin2(){
+    document.getElementById("gambarSungjin").src="assets/image/sungjin2.jpg";
+}
+
+function changeGambarBrian1(){
+    document.getElementById("gambarBrian").src="assets/image/youngk1.jpg";
+}
+
+function changeGambarBrian2(){
+    document.getElementById("gambarBrian").src="assets/image/youngk2.jpg";
+}
+
+function changeGambarWonpil1(){
+    document.getElementById("gambarWonpil").src="assets/image/wonpil1.jpg";
+}
+
+function changeGambarWonpil2(){
+    document.getElementById("gambarWonpil").src="assets/image/wonpil2.jpg";
+}
+
+function changeGambarDowoon1(){
+    document.getElementById("gambarDowoon").src="assets/image/dowoon1.jpg";
+}
+
+function changeGambarDowoon2(){
+    document.getElementById("gambarDowoon").src="assets/image/dowoon2.jpg";
+}
